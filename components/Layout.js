c24997ef928d380a56580eddb66a94627fed8f0d
Luka Sheklashvili <Luka.sheylashvili@gmail.com>
2021-11-14 17:50:47 +0400

    Content

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import styles from "../styles/Layout.module.css";
+import Header from "./Header";
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <Header />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
